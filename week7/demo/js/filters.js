b571d009361fa234c76eb28bedb1cd67c8fe04bf
GFORTI <GFORTI@N210INSTR.labs.neit.edu>
2015-05-05 17:40:33 -0400

    MVC

--- week7/demo/js/filters.js
+++ week7/demo/js/filters.js
@@ -2,21 +2,8 @@
 
 var appFilters = angular.module('appFilters', []);
 
-appFilters.filter('findPhone', function() {
-    return function(arr, id) {
-        if ( !angular.isArray(arr) || !arr.length || !angular.isString(id) ) return arr;
-        var results = {};
-        
-        // it's more
-        angular.forEach(arr, function (value, key) {
-            if ( !results.length ) {
-                if ( value.hasOwnProperty('id') && value.id === id ) {
-                    results = angular.copy(value);
-                }
-            }
-        }, results);
-        
-        
-        return results;
+appFilters.filter('activeString', function() {
+    return function(active) {        
+        return ( !!active ? 'Yes' : 'No');
     };
 });
